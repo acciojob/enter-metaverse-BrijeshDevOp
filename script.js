365464f7ddd283112cb0be0ea3f7895e281d22ae
BrijeshDevOp <160261475+BrijeshDevOp@users.noreply.github.com>
2026-08-13 21:54:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const p = document.getElementById("status");
+const h1 = document.createElement("h1");
+const btn = document.getElementById("enterBtn");
+
+let content = "Entered Metaverse";
+
+btn.addEventListener("click",function() {
+	h1.innerHtml = p.innerHtml;
+	p.replaceWith(h1);
+});
